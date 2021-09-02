19c1e4c960de93e9c48450d7ec3b7a85aba74588
Samuel Roberts <samuel.roberts@mac.com>
2021-09-02 22:39:39 +0100

    Lesson 1: Part 6

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,53 @@
 
 use image::{RgbaImage, Rgba};
+use obj::{Obj};
 
+#[derive(Copy, Clone)]
 struct Point
 {
     x: i32,
     y: i32
 }
 
+const WIDTH: u32 = 1000;
+const HEIGHT: u32 = 1000;
+
 fn main() {
-    const WIDTH: u32 = 100;
-    const HEIGHT: u32 = 100;
 
     let mut image = RgbaImage::new(WIDTH, HEIGHT);
-    
-    line(&mut image, Point { x: 13, y: 20 }, Point { x: 80, y: 40 }, Rgba([0, 0, 255, 255])); 
-    line(&mut image, Point { x: 20, y: 13 }, Point { x: 40, y: 80 }, Rgba([0, 255, 0, 255])); 
-    line(&mut image, Point { x: 80, y: 80 }, Point { x: 13, y: 13 }, Rgba([255, 0, 0, 255]));
+
+    let data = Obj::load("test.obj").unwrap().data;
+
+    for object in &data.objects {
+        for group in &object.groups {
+            for polygon in &group.polys {
+                
+                // Ignore polygons that are not triangles
+                if polygon.0.len() != 3 {
+                    continue;
+                }
+
+                // Get the indicies of the points of the triangle
+                let idx0 = polygon.0[0].0;
+                let idx1 = polygon.0[1].0;
+                let idx2 = polygon.0[2].0;
+
+                let indices = vec!(idx0, idx1, idx2);
+                let positions: Vec<_> = indices
+                    .iter()
+                    .map(|i| data.position[*i])
+                    .map(|p| Point { 
+                        x: (((p[0] + 1.0) * ((WIDTH as f32) / 2.0)) as i32), 
+                        y: (((p[1] + 1.0) * ((HEIGHT as f32) / 2.0)) as i32) 
+                    })
+                    .collect();
+
+                line(&mut image, positions[0], positions[1], Rgba([0, 0, 0, 255]));
+                line(&mut image, positions[1], positions[2], Rgba([0, 0, 0, 255]));
+                line(&mut image, positions[2], positions[0], Rgba([0, 0, 0, 255]));
+            }
+        }
+    }
 
     image.save("output.png").expect("Failed to save image");
 }
@@ -48,6 +80,10 @@ fn line(image: &mut RgbaImage, p0: Point, p1: Point, colour: Rgba<u8>) {
     let mut y: i32 = y0;
 
     for x in x0..=x1 {
+        if x < 0 || x as u32 >= WIDTH || y < 0 || y as u32 >= HEIGHT {
+            continue;
+        }
+
         if steep {
             image.put_pixel(y as u32, x as u32, colour);
         } else {

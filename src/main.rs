2cc791e26eb4e9372059074584943d6023c257b2
Samuel Roberts <samuel.roberts@mac.com>
2021-10-27 21:20:59 +0100

    Created a mesh struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use image::Rgba;
+
+use drawable::Drawable;
 use nalgebra::{Matrix4, Point3, Vector3};
-use obj::Obj;
 
 mod renderer;
 mod utilities;
+mod mesh;
+mod drawable;
 
 use renderer::Renderer;
+use mesh::Mesh;
 
 const WIDTH: u32 = 1000;
 const HEIGHT: u32 = 1000;
@@ -13,7 +16,6 @@ const HEIGHT: u32 = 1000;
 fn main() {
     let mut renderer = Renderer::new(WIDTH, HEIGHT);
 
-    let model = Matrix4::<f32>::identity();
     let view = Matrix4::<f32>::look_at_lh(
         &Point3::<f32>::new(25.0, 25.0, 25.0),
         &Point3::<f32>::origin(),
@@ -26,44 +28,11 @@ fn main() {
         1e5,
     );
 
-    renderer.push_model_matrix(model);
     renderer.push_view_matrix(view);
     renderer.push_projection_matrix(projection);
 
-    let data = Obj::load("models/calibration.obj").unwrap().data;
-
-    for object in &data.objects {
-        for group in &object.groups {
-            for polygon in &group.polys {
-                // Ignore polygons that are not triangles
-                if polygon.0.len() != 3 {
-                    continue;
-                }
-
-                // Get the indicies of the points of the triangle
-                let idx0 = polygon.0[0].0;
-                let idx1 = polygon.0[1].0;
-                let idx2 = polygon.0[2].0;
-
-                let indices = vec![idx0, idx1, idx2];
-                let positions: Vec<_> = indices
-                    .iter()
-                    .map(|i| data.position[*i])
-                    .map(|p| Point3::<f32>::new(p[0], p[1], p[2]))
-                    .collect();
-
-                //renderer.line(positions[0], positions[1], Rgba([0, 0, 0, 255]));
-                //renderer.line(positions[1], positions[2], Rgba([0, 0, 0, 255]));
-                //renderer.line(positions[2], positions[0], Rgba([0, 0, 0, 255]));
-                renderer.triangle(
-                    positions[0],
-                    positions[1],
-                    positions[2],
-                    Rgba([255, 255, 255, 255]),
-                );
-            }
-        }
-    }
+    let mesh = Mesh::load_obj("models/calibration.obj").expect("Failed to load model");
+    mesh.draw(&mut renderer);
 
     renderer.save("output.png").unwrap();
 }
---
+++ src/mesh.rs
@@ -0,0 +1,72 @@
+
+use image::Rgba;
+use nalgebra::{Matrix4, Point3, Vector3};
+use obj::Obj;
+
+use crate::drawable::Drawable;
+use crate::renderer::Renderer;
+
+struct Triangle {
+    a: Point3<f32>,
+    b: Point3<f32>,
+    c: Point3<f32>,
+    colour: Rgba<u8>
+}
+
+pub struct Mesh {
+    geometry: Vec<Triangle>,
+    transform: Matrix4<f32>
+}
+
+impl Mesh {
+    pub fn load_obj(filename: &str) -> Result<Mesh, String> {
+
+        let data = Obj::load(filename).unwrap().data;
+        let mut triangles = Vec::<Triangle>::new();
+
+        for object in &data.objects {
+            for group in &object.groups {
+                for polygon in &group.polys {
+                    // Ignore polygons that are not triangles
+                    if polygon.0.len() != 3 {
+                        continue;
+                    }
+    
+                    // Get the indicies of the points of the triangle
+                    let idx0 = polygon.0[0].0;
+                    let idx1 = polygon.0[1].0;
+                    let idx2 = polygon.0[2].0;
+    
+                    let indices = vec![idx0, idx1, idx2];
+                    let positions: Vec<_> = indices
+                        .iter()
+                        .map(|i| data.position[*i])
+                        .map(|p| Point3::<f32>::new(p[0], p[1], p[2]))
+                        .collect();
+    
+                    triangles.push(Triangle {
+                        a: positions[0],
+                        b: positions[1],
+                        c: positions[2],
+                        colour: Rgba([255, 255, 255, 255]),
+                    });
+                }
+            }
+        }
+
+        return Ok(Mesh {
+            geometry: triangles,
+            transform: Matrix4::<f32>::identity()
+        });
+    }
+}
+
+impl Drawable for Mesh {
+    fn draw(&self, renderer: &mut Renderer) {
+        renderer.push_model_matrix(self.transform);
+        for triangle in &self.geometry {
+            renderer.triangle(triangle.a, triangle.b, triangle.c, triangle.colour);
+        }
+        renderer.pop_model_matrix();
+    }
+}
